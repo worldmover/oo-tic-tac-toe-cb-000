20d5e5e02245bf5c2e79674f108ea3cc9b9c6562
Christian Weiss <worldmover777@gmail.com>
2018-02-25 14:44:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,8 @@ class TicTacToe
      @board[index] = token
    end
 
-   def position_taken?(board,index)
-   if board[index]!=" "
+   def position_taken?(@board,index)
+   if @board[index]!=" "
      return true
    end
    return false
