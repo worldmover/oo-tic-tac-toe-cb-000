8bacde15170159e6c4a6638b3af5bcd5f1458e61
Christian Weiss <worldmover777@gmail.com>
2018-02-25 14:55:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,4 +67,16 @@ def turn
   end
 end
 
+def won?
+  WIN_COMBINATIONS.each do |win|
+    if @board[win[0]] == "X" && @board[win[1]] == "X" && @board[win[2]] == "X"
+      return win
+    end
+    if @board[win[0]] == "O" && @board[win[1]] == "O" && @board[win[2]] == "O"
+      return win
+    end
+  end
+  return false
+end
+
 end
